9cda450c2ad356b90a545734d6fde35cdb87506f
Nathaniel Case <Qalthos@gmail.com>
2013-10-13 11:14:31 -0400

    Remove all Dwolla. Revert to undo, please.

--- roulette/site.py
+++ roulette/site.py
@@ -1,9 +1,9 @@
 from flask import Flask, url_for, redirect, render_template, request, session
 import scrape
 
-from dwolla import DwollaClientApp, DwollaUser
+#from dwolla import DwollaClientApp, DwollaUser
 
-Dwolla = DwollaClientApp("Q/9GH5LwLTp06+VUj7Rt9iKFgyIOQmFZF0AxTEAYyWoC84P4YN", "ePRDZwcOOTjPbZ5rdyRvZpg6GMpHrUyxyQLNQzowFMiUtov2g")
+#Dwolla = DwollaClientApp("Q/9GH5LwLTp06+VUj7Rt9iKFgyIOQmFZF0AxTEAYyWoC84P4YN", "ePRDZwcOOTjPbZ5rdyRvZpg6GMpHrUyxyQLNQzowFMiUtov2g")
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -13,7 +13,7 @@ app.secret_key = "key"
 def index():
     oauth_return_url = url_for('dwolla_oauth_return', _external=True)#point back to this file
     permissions = 'send'
-    auth_url = Dwolla.init_oauth_url(oauth_return_url, permissions)
+    auth_url = ''#Dwolla.init_oauth_url(oauth_return_url, permissions)
     url = None
     title = None
     if request.args.get("search"):
@@ -57,19 +57,19 @@ def finish_songs():
 #STEP:2 = temp code exchange
 @app.route("/dwolla/oauth_return")
 def dwolla_oauth_return():
-    oauth_return_url = url_for('dwolla_oauth_return', external=True)#print back to this file/URL
-    code = request.args.get("code")
-    token = Dwolla.get_oauth_token(code, redirect_uri=oauth_return_url)
-    session['token'] = token
-    return 'Your never-expiering OAuth access token is: <b>%s</b>' % token
+    #oauth_return_url = url_for('dwolla_oauth_return', external=True)#print back to this file/URL
+    #code = request.args.get("code")
+    #token = Dwolla.get_oauth_token(code, redirect_uri=oauth_return_url)
+    #session['token'] = token
+    return 'Your never-expiering OAuth access token is: <b>%s</b>' #% token
 
 
 #STEP 3: Take Token and swap it into the right place
 @app.route("/dwolla/donate")
 def donate():
-    user = DwollaUser(session['token'])
-    transactionId = user.send_funds(0.00, 'phone #', '[PIN]')
-    print transactionId
+    #user = DwollaUser(session['token'])
+    #transactionId = user.send_funds(0.00, 'phone #', '[PIN]')
+    print #transactionId
 
 if __name__ == '__main__':
     app.debug = True
