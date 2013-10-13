309517ed4cbcd2d4948eb233622c90379122bbd9
David Wilson <daw4475@g.rit.edu>
2013-10-13 01:04:39 -0400

    Module updates

--- roulette/site.py
+++ roulette/site.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import scrape
 
 
 app = Flask(__name__)
+app.secret_key = "key"
 
 
 @app.route('/')
 def bandcampRoulette():
 
-    info = [None]
+    info = None
     if request.args.get("search"):
         info = scrape.scrape(request.args.get("search"))
-    return render_template("index.html", song=info[0])
+        if info is not None:
+            session['songs'] = [info[0], info[1]]
+            print(session['songs'])
+            return render_template("index.html", song=info[0], session=session)
+    return render_template("index.html", song=info)
 
 if __name__ == '__main__':
     app.debug = True
