451f64ee8149fb439eb0bd8e092c0579725caf8c
Ryan <ras8502@rit.edu>
2013-10-13 05:52:38 -0400

    added dwolla code into site.py

--- roulette/site.py
+++ roulette/site.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, url_for, redirect, render_template, request, session
 import scrape
 
+from dwolla import DwollaClientApp
+
+from dwolla import DwollaUser
+
+
+Dwolla = DwollaClientApp("Q/9GH5LwLTp06+VUj7Rt9iKFgyIOQmFZF0AxTEAYyWoC84P4YN", "ePRDZwcOOTjPbZ5rdyRvZpg6GMpHrUyxyQLNQzowFMiUtov2g")
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -9,6 +15,10 @@ app.secret_key = "key"
 @app.route('/')
 def bandcampRoulette():
 
+
+    oauth_return_url = url_for('dwolla_oauth_return', _external=True)#point back to this file
+    permissions = 'send'
+    auth_url = Dwolla.init_oauth_url(oauth_return_url, permissions)
     url = None
     title = None
     if request.args.get("search"):
@@ -22,7 +32,7 @@ def bandcampRoulette():
             print(e)
             pass
 
-    return render_template("index.html", song=url, title=title)
+    return render_template("index.html", song=url, title=title, auth_url=auth_url)
 
 
 @app.route('/like')
@@ -36,6 +46,23 @@ def addSong():
         session['liked_songs'][url] = title
         return "Song added"
     return "Song not added"
+
+#STEP:2 = temp code exchange
+@app.route("/dwolla/oauth_return")
+def dwolla_oauth_return():
+    oauth_return_url = url_for('dwolla_oauth_return', external=True)#print back to this file/URL
+    code = request.args.get("code")
+    token = Dwolla.get_oauth_token(code, redirect_uri=oauth_return_url)
+    session['token'] = token
+    return 'Your never-expiering OAuth access token is: <b>%s</b>' % token
+
+
+#STEP 3: Take Token and swap it into the right place
+@app.route("/dwolla/donate")
+def donate():
+    user = DwollaUser(session['token'])
+    transactionId = user.send_funds(0.00, 'phone #', '[PIN]')
+    print transactionId
 
 if __name__ == '__main__':
     app.debug = True
